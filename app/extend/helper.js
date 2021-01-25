35ff445d139e4804bcd9b1baa2a30f0fa93dc830
zhengzhichao <377950622@qq.com>
2021-01-25 14:55:26 +0800

    智慧园区-项目

--- app/extend/helper.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const moment = require('moment');
-
-moment.locale('zh-CN');
-exports.relativeTime = time => moment(time).fromNow();
--- app/router.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  require('./router/home')(app);
-  require('./router/news')(app);
-  require('./router/upload')(app);
-  require('./router/mysql')(app);
-};
